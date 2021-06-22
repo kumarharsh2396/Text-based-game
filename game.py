e3e31a3a2a8f1d6d42ade4826ff77865e1b0ce1d
kumarharsh2396 <kumarharsh2396@gmail.com>
2021-06-22 16:30:29 +0530

    function for newgame

--- game.py
+++ game.py
@@ -42,3 +42,130 @@ def chooseGenre(beenHere):
 	# Add more elif statements here	
 	else:
 		 return chooseGenre(True)	
+
+
+# Function for a new game
+def newGame():
+
+	genre = chooseGenre(False)
+	itemList = generateList(genre)
+	
+	# If Items loaded into itemList, proceed to play. Else, abort.
+	if len(itemList) > 0:
+		print("Loaded List Successfully. Proceed to play the game")
+	else:
+		print("Error loading items into the list. Aborting!")
+		exit()
+
+	# A list that keeps track of the items mentioned in the game
+	gameList = []
+
+	# Game continues until the itemList is exhausted
+	while len(itemList) > 0:
+
+		# Player's turn
+		click.clear()
+		print(genre.upper() + "\n")
+		print("Player's Turn\n")
+
+		if len(gameList) > 0:
+			print("Enter the list itemwise, in order, as you remember it. If you forget any of the items, type in 'idk' in its place.")
+
+		# List to maintain the entries of the player	
+		playerList = []
+
+		# Ask the player to enter all the items mentioned so far
+		for i in range(len(gameList)):
+			playerEntry = input("Item " + str(i+1) + ": ").lower()
+			# Break out, incase the player forgets in the middle
+			if playerEntry == "idk":
+				break
+			# If an entry is made, append it to the playerList	
+			playerList.append(playerEntry)
+
+		# Checking if the player remebers the items correctly:
+		areSame = True
+		# If the length of both the lists are same and the contents are also the same, the player gets to proceed.
+		if len(gameList) == len(playerList):
+			for i in range(len(gameList)):
+				if playerList[i] != gameList[i]:
+					areSame = False
+		# If the length of the lists are different, the player had typed an idk in the middle. So end the game.			
+		else:
+			areSame = False		
+
+		# If the lengths were the same but the list items were different, then also end the game.	
+		if not areSame:
+			break
+
+		# Motivate the player for remebering right
+		if len(gameList) > 0:
+			print("\nGood job! You remember the list right.\n")
+
+		# Prompt player to add new item to the list	
+		newInput = input("Add a new item to the list: ").lower()
+
+		# Check if the entry is valid for given genre or if the
+
+		knowsMore = True
+		while newInput not in itemList:
+			print("Sorry! That item is invalid! Enter a valid item or enter 'idk' if you don't know any more items:")
+			newInput = input()
+			if newInput == "idk":
+				knowsMore = False
+				break
+
+		if not knowsMore:
+			break
+
+		print("\n'" + newInput.upper() +"' added to the list!")	
+
+		# Once a valid input is made, remove it from itemList, so it cannot be repeated. Add it to gameList so it is in play.
+		itemList.remove(newInput)
+		gameList.append(newInput)
+
+
+		# Print updated List
+		print("\nThe updated list is:\n")
+		for item in gameList:
+			print(item)
+
+		print("\nHit enter to continue.")
+		input()
+
+		# Computer's turn
+
+		click.clear()
+		print("FRUITS\n")
+		print("Computer's Turn\n")
+
+		# Make a random choice from the ones available and add it to the gameList. Also, remove from itemList so that it can't be repeated
+		choiceIndex = random.randrange(0, len(itemList))
+		gameList.append(itemList[choiceIndex])
+		itemList.remove(itemList[choiceIndex])
+
+		# Prompt the user of the new item.
+		print("Computer adds '" + gameList[-1].upper() + "' to the list. This list is now:\n")
+
+		# Print the updated list for them to memorize
+		for item in gameList:
+			print(item)
+
+		print("\nMemorize this list. Hit enter when you're ready!")
+		input()
+
+	# If by some miracle, they manage to empty the itemList, they win. 
+	if len(itemList) == 0:
+		print("\nYou Won!")
+	elif not knowsMore:	
+		print("\nYou could have said " + itemList[random.randrange(0, len(itemList))].upper() + ". ;)")
+	else:
+		print("\nSorry! Looks like you forgot! The list was:\n")
+		for item in gameList:
+			print(item)
+		
+	print("\nGood Run! Your score is " + str(len(gameList)) + ".\n")	
+
+	input("Press enter to return to main menu")	
+
+	
